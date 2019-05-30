440bb6e7c86886108a32f369af004ac750f66969
chhzh123 <30967989+chhzh123@users.noreply.github.com>
2019-05-30 21:16:14 +0800

    [Python] Update ps1-2
    1. Update ps1-2
    2. Change folder ps0-2 to ps1-2

---
+++ AdvancedComputerProgramming/ps2-2/graph.py
@@ -0,0 +1,169 @@
+# 6.0002 Problem Set 5
+# Graph optimization
+# Name: Hongzheng Chen 17341015
+
+import unittest
+
+#
+# A set of data structures to represent graphs
+#
+
+class Node(object):
+    """Represents a node in the graph"""
+    def __init__(self, name):
+        self.name = str(name)
+
+    def get_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        # This function is necessary so that Nodes can be used as
+        # keys in a dictionary, even though Nodes are mutable
+        return self.name.__hash__()
+
+
+class Edge(object):
+    """Represents an edge in the dictionary. Includes a source and
+    a destination."""
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+
+    def get_source(self):
+        return self.src
+
+    def get_destination(self):
+        return self.dest
+
+    def __str__(self):
+        return '{}->{}'.format(self.src, self.dest)
+
+
+class WeightedEdge(Edge):
+    def __init__(self, src, dest, total_distance, outdoor_distance):
+        Edge.__init__(self,src,dest)
+        self.total_distance = total_distance
+        self.outdoor_distance = outdoor_distance
+
+    def get_total_distance(self):
+        return self.total_distance
+
+    def get_outdoor_distance(self):
+        return self.outdoor_distance
+
+    def __str__(self):
+        return '{}->{} ({}, {})'.format(self.src,self.dest,self.total_distance,self.outdoor_distance)
+
+
+class Digraph(object):
+    """Represents a directed graph of Node and Edge objects"""
+    def __init__(self):
+        self.nodes = set([])
+        self.edges = {}  # must be a dict of Node -> list of edges
+
+    def __str__(self):
+        edge_strs = []
+        for edges in self.edges.values():
+            for edge in edges:
+                edge_strs.append(str(edge))
+        edge_strs = sorted(edge_strs)  # sort alphabetically
+        return '\n'.join(edge_strs)  # concat edge_strs with "\n"s between them
+
+    def get_edges_for_node(self, node):
+        return self.edges[node]
+
+    def has_node(self, node):
+        return node in self.nodes
+
+    def add_node(self, node):
+        """Adds a Node object to the Digraph. Raises a ValueError if it is
+        already in the graph."""
+        if self.has_node(node):
+            raise ValueError
+        else:
+            self.nodes.add(node)
+
+    def add_edge(self, edge):
+        """Adds an Edge or WeightedEdge instance to the Digraph. Raises a
+        ValueError if either of the nodes associated with the edge is not
+        in the  graph."""
+        src = Node(edge.get_source())
+        dst = Node(edge.get_destination())
+        if self.has_node(src) and self.has_node(dst):
+            if self.edges.get(src,0) == 0:
+                self.edges[src] = [edge]
+            else:
+                self.edges[src].append(edge)
+        else:
+            raise ValueError
+
+
+# ================================================================
+# Begin tests -- you do not need to modify anything below this line
+# ================================================================
+
+class TestGraph(unittest.TestCase):
+
+    def setUp(self):
+        self.g = Digraph()
+        self.na = Node('a')
+        self.nb = Node('b')
+        self.nc = Node('c')
+        self.g.add_node(self.na)
+        self.g.add_node(self.nb)
+        self.g.add_node(self.nc)
+        self.e1 = WeightedEdge(self.na, self.nb, 15, 10)
+        self.e2 = WeightedEdge(self.na, self.nc, 14, 6)
+        self.e3 = WeightedEdge(self.nb, self.nc, 3, 1)
+        self.g.add_edge(self.e1)
+        self.g.add_edge(self.e2)
+        self.g.add_edge(self.e3)
+
+    def test_weighted_edge_str(self):
+        self.assertEqual(str(self.e1), "a->b (15, 10)")
+        self.assertEqual(str(self.e2), "a->c (14, 6)")
+        self.assertEqual(str(self.e3), "b->c (3, 1)")
+
+    def test_weighted_edge_total_distance(self):
+        self.assertEqual(self.e1.get_total_distance(), 15)
+        self.assertEqual(self.e2.get_total_distance(), 14)
+        self.assertEqual(self.e3.get_total_distance(), 3)
+
+    def test_weighted_edge_outdoor_distance(self):
+        self.assertEqual(self.e1.get_outdoor_distance(), 10)
+        self.assertEqual(self.e2.get_outdoor_distance(), 6)
+        self.assertEqual(self.e3.get_outdoor_distance(), 1)
+
+    def test_add_edge_to_nonexistent_node_raises(self):
+        node_not_in_graph = Node('q')
+        no_src = WeightedEdge(self.nb, node_not_in_graph, 5, 5)
+        no_dest = WeightedEdge(node_not_in_graph, self.na, 5, 5)
+
+        with self.assertRaises(ValueError):
+            self.g.add_edge(no_src)
+        with self.assertRaises(ValueError):
+            self.g.add_edge(no_dest)
+
+    def test_add_existing_node_raises(self):
+        with self.assertRaises(ValueError):
+            self.g.add_node(self.na)
+
+    def test_graph_str(self):
+        expected = "a->b (15, 10)\na->c (14, 6)\nb->c (3, 1)"
+        self.assertEqual(str(self.g), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ AdvancedComputerProgramming/ps2-2/ps2.py
@@ -0,0 +1,264 @@
+# 6.0002 Problem Set 5
+# Graph optimization
+# Name: Hongzheng Chen 17341015
+
+#
+# Finding shortest paths through MIT buildings
+#
+import unittest
+from graph import Digraph, Node, WeightedEdge
+import copy
+
+#
+# Problem 2: Building up the Campus Map
+#
+# Problem 2a: Designing your graph
+#
+# What do the graph's nodes represent in this problem? What
+# do the graph's edges represent? Where are the distances
+# represented?
+#
+# Answer: The nodes represent the buildings, and the edges
+#         represent there exists a road between one building to another.
+#         The distance represent how far getting from the
+#         source building to the destination.
+#
+
+
+# Problem 2b: Implementing load_map
+def load_map(map_filename):
+    """
+    Parses the map file and constructs a directed graph
+
+    Parameters:
+        map_filename : name of the map file
+
+    Assumes:
+        Each entry in the map file consists of the following four positive
+        integers, separated by a blank space:
+            From To TotalDistance DistanceOutdoors
+        e.g.
+            32 76 54 23
+        This entry would become an edge from 32 to 76.
+
+    Returns:
+        a Digraph representing the map
+    """
+    print("Loading map from file...")
+    g = Digraph()
+    with open(map_filename,"r") as file:
+        for line in file:
+            (src,dst,tot_dist,outdoor_dist) = line.split(' ')
+            tot_dist = int(tot_dist)
+            outdoor_dist = int(outdoor_dist)
+            if not g.has_node(Node(src)):
+                g.add_node(Node(src))
+            if not g.has_node(Node(dst)):
+                g.add_node(Node(dst))
+            g.add_edge(WeightedEdge(src,dst,tot_dist,outdoor_dist))
+    return g
+
+# Problem 2c: Testing load_map
+# Include the lines used to test load_map below, but comment them out
+
+# school_map = load_map("test_load_map.txt")
+# print(school_map)
+
+#
+# Problem 3: Finding the Shorest Path using Optimized Search Method
+#
+# Problem 3a: Objective function
+#
+# What is the objective function for this problem? What are the constraints?
+#
+# Answer:
+# The objective function is to minimize the distance from one given building to another.
+# The constraint is not to exceed the maximum distance outdoors.
+#
+
+# Problem 3b: Implement get_best_path
+def get_best_path(digraph, start, end, path, max_dist_outdoors, best_dist,
+                  best_path):
+    """
+    Finds the shortest path between buildings subject to constraints.
+
+    Parameters:
+        digraph: Digraph instance
+            The graph on which to carry out the search
+        start: string
+            Building number at which to start
+        end: string
+            Building number at which to end
+        path: list composed of [[list of strings], int, int]
+            Represents the current path of nodes being traversed. Contains
+            a list of node names, total distance traveled, and total
+            distance outdoors.
+        max_dist_outdoors: int
+            Maximum distance spent outdoors on a path
+        best_dist: int
+            The smallest distance between the original start and end node
+            for the initial problem that you are trying to solve
+        best_path: list of strings
+            The shortest path found so far between the original start
+            and end node.
+
+    Returns:
+        A tuple with the shortest-path from start to end, represented by
+        a list of building numbers (in strings), [n_1, n_2, ..., n_k],
+        where there exists an edge from n_i to n_(i+1) in digraph,
+        for all 1 <= i < k and the distance of that path.
+
+        If there exists no path that satisfies max_total_dist and
+        max_dist_outdoors constraints, then return None.
+    """
+    # not valid nodes
+    if not (digraph.has_node(Node(start)) and digraph.has_node(Node(end))):
+        raise ValueError
+    # recursion termination
+    elif start == end:
+        if path[1] < best_dist:
+            best_path = copy.deepcopy(path[0])
+            best_dist = path[1]
+    # get deeper
+    else:
+        for edge in digraph.get_edges_for_node(Node(start)):
+            dst = edge.get_destination()
+            if dst in path[0]: # avoid cycles
+                continue
+            tmp_path = copy.deepcopy(path)
+            tmp_path[0].append(dst)
+            tmp_path[1] += edge.get_total_distance()
+            tmp_path[2] += edge.get_outdoor_distance()
+            if tmp_path[2] > max_dist_outdoors: # not satisfy the constraint
+                continue
+            if tmp_path[1] > best_dist: # cut!!! greatly reduce time
+                continue
+            (tmp_best_path,tmp_best_dist) = get_best_path(digraph,dst,end,
+                tmp_path,max_dist_outdoors,best_dist,best_path)
+            if tmp_best_dist < best_dist: # update best path
+                best_path = tmp_best_path
+                best_dist = tmp_best_dist
+    return (best_path,best_dist)
+
+# Problem 3c: Implement directed_dfs
+def directed_dfs(digraph, start, end, max_total_dist, max_dist_outdoors):
+    """
+    Finds the shortest path from start to end using a directed depth-first
+    search. The total distance traveled on the path must not
+    exceed max_total_dist, and the distance spent outdoors on this path must
+    not exceed max_dist_outdoors.
+
+    Parameters:
+        digraph: Digraph instance
+            The graph on which to carry out the search
+        start: string
+            Building number at which to start
+        end: string
+            Building number at which to end
+        max_total_dist: int
+            Maximum total distance on a path
+        max_dist_outdoors: int
+            Maximum distance spent outdoors on a path
+
+    Returns:
+        The shortest-path from start to end, represented by
+        a list of building numbers (in strings), [n_1, n_2, ..., n_k],
+        where there exists an edge from n_i to n_(i+1) in digraph,
+        for all 1 <= i < k
+
+        If there exists no path that satisfies max_total_dist and
+        max_dist_outdoors constraints, then raises a ValueError.
+    """
+    (best_path,best_dist) = get_best_path(digraph,start,end,[[start],0,0],max_dist_outdoors,max_total_dist,[])
+    if best_path == []:
+        raise ValueError
+    else:
+        return best_path
+
+
+# ================================================================
+# Begin tests -- you do not need to modify anything below this line
+# ================================================================
+
+class Ps2Test(unittest.TestCase):
+    LARGE_DIST = 99999
+
+    def setUp(self):
+        self.graph = load_map("mit_map.txt")
+
+    def test_load_map_basic(self):
+        self.assertTrue(isinstance(self.graph, Digraph))
+        self.assertEqual(len(self.graph.nodes), 37)
+        all_edges = []
+        for _, edges in self.graph.edges.items():
+            all_edges += edges  # edges must be dict of node -> list of edges
+        all_edges = set(all_edges)
+        self.assertEqual(len(all_edges), 129)
+
+    def _print_path_description(self, start, end, total_dist, outdoor_dist):
+        constraint = ""
+        if outdoor_dist != Ps2Test.LARGE_DIST:
+            constraint = "without walking more than {}m outdoors".format(
+                outdoor_dist)
+        if total_dist != Ps2Test.LARGE_DIST:
+            if constraint:
+                constraint += ' or {}m total'.format(total_dist)
+            else:
+                constraint = "without walking more than {}m total".format(
+                    total_dist)
+
+        print("------------------------")
+        print("Shortest path from Building {} to {} {}".format(
+            start, end, constraint))
+
+    def _test_path(self,
+                   expectedPath,
+                   total_dist=LARGE_DIST,
+                   outdoor_dist=LARGE_DIST):
+        start, end = expectedPath[0], expectedPath[-1]
+        self._print_path_description(start, end, total_dist, outdoor_dist)
+        dfsPath = directed_dfs(self.graph, start, end, total_dist, outdoor_dist)
+        print("Expected: ", expectedPath)
+        print("DFS: ", dfsPath)
+        self.assertEqual(expectedPath, dfsPath)
+
+    def _test_impossible_path(self,
+                              start,
+                              end,
+                              total_dist=LARGE_DIST,
+                              outdoor_dist=LARGE_DIST):
+        self._print_path_description(start, end, total_dist, outdoor_dist)
+        with self.assertRaises(ValueError):
+            directed_dfs(self.graph, start, end, total_dist, outdoor_dist)
+
+    def test_path_one_step(self):
+        self._test_path(expectedPath=['32', '56'])
+
+    def test_path_no_outdoors(self):
+        self._test_path(
+            expectedPath=['32', '36', '26', '16', '56'], outdoor_dist=0)
+
+    def test_path_multi_step(self):
+        self._test_path(expectedPath=['2', '3', '7', '9'])
+
+    def test_path_multi_step_no_outdoors(self):
+        self._test_path(
+            expectedPath=['2', '4', '10', '13', '9'], outdoor_dist=0)
+
+    def test_path_multi_step2(self):
+        self._test_path(expectedPath=['1', '4', '12', '32'])
+
+    def test_path_multi_step_no_outdoors2(self):
+        self._test_path(
+            expectedPath=['1', '3', '10', '4', '12', '24', '34', '36', '32'],
+            outdoor_dist=0)
+
+    def test_impossible_path1(self):
+        self._test_impossible_path('8', '50', outdoor_dist=0)
+
+    def test_impossible_path2(self):
+        self._test_impossible_path('10', '32', total_dist=100)
+
+
+if __name__ == "__main__":
+    unittest.main()
