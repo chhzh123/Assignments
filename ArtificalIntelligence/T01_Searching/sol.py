368df9e7c11b9a86ae5320b352212c25f110d0d3
chhzh123 <30967989+chhzh123@users.noreply.github.com>
2019-10-08 14:17:53 +0800

    [AI] Update T01 answer

--- ArtificalIntelligence/T01_Searching/sol.py
+++ ArtificalIntelligence/T01_Searching/sol.py
@@ -4,17 +4,18 @@ from queue import PriorityQueue
 M = 5
 K = 3
 q = PriorityQueue()
-q.put((8,1,5,5,1,0,(-1,-1,-1)))
+q.put(((8,8),1,5,5,1,0,(-1,-1,-1)))
 cycle = [(5,5,1)]
 res = []
 
 cnt = 1
 while not q.empty():
 	p, curr, m, c, b, step, prev = q.get()
-	for i in range(step):
-		print("\t",end="")
-	# print(m,c,b,"({}{}[{}])".format(prev,p,curr))
-	print(m,c,b,"({})".format(p))
+	p = p[0]
+	# for i in range(step):
+	# 	print("\t",end="")
+	# # print(m,c,b,"({}{}[{}])".format(prev,p,curr))
+	# print(m,c,b,"({})".format(p))
 	# print("i",m,c,b,"({})".format(p))
 	res.append((p,curr,m,c,b,step))
 	if m == 0 and c == 0 and b == 0:
@@ -27,7 +28,7 @@ while not q.empty():
 		# 	print(m,c,b,"({}[{}])".format(p,curr))
 		sys.exit()
 	for pair in [(0,1),(0,2),(0,3),(1,0),(1,1),(2,0),(2,1),(3,0)]: # boat ensure
-		if pair[0] > m or pair[1] > c:
+		if (b == 1 and (pair[0] > m or pair[1] > c)) or (b == 0 and (pair[0] > 5 - m or pair[1] > 5 - c)):
 			continue
 		if b == 0:
 			newM = m + pair[0]
@@ -37,19 +38,20 @@ while not q.empty():
 			newM = m - pair[0]
 			newC = c - pair[1]
 			newB = 0
-		if (newM != 0 and newM < newC) or (5 - newM != 0 and 5 - newM < 5 - newC):
+		if (newM != 0 and newM < newC) or (5 - newM != 0 and 5 - newM < 5 - newC) or newM < 0 or newC < 0 or newM > M or newC > M:
 			continue
 		priority = step + 1 + newM + newC - 2 * newB
 		cnt += 1
 		if (newM,newC,newB) not in cycle:
-			# for i in range(step+1):
-			# 	print("\t",end="")
-			# print(newM,newC,newB,"({}[{}])".format(priority,cnt))
-			cycle.append((newM,newC,newB))
-			q.put((priority,cnt,newM,newC,newB,step+1,(m,c,b)))
-		else:
 			for i in range(step+1):
 				print("\t",end="")
-			# print(newM,newC,newB,"({}d[{}])".format((m,c,b),cnt))
-			print(newM,newC,newB,"(d)")
+			# print(newM,newC,newB,"({}[{}])".format(priority,cnt))
+			print(newM,newC,newB,"({})".format(priority))
+			cycle.append((newM,newC,newB))
+			q.put(((priority,newM+newC-2*newB),cnt,newM,newC,newB,step+1,(m,c,b)))
+		else:
+			# for i in range(step+1):
+			# 	print("\t",end="")
+			# # print(newM,newC,newB,"({}d[{}])".format((m,c,b),cnt))
+			# print(newM,newC,newB,"(d)")
 			res.append((0,cnt,newM,newC,newB,step+1))
