608c0a8a4f4d9c837a3d8908ca5e871e0d1886a1
chhzh123 <30967989+chhzh123@users.noreply.github.com>
2019-10-12 10:56:51 +0800

    [AI] Update E06 - Prolog Queries (report)

--- ArtificalIntelligence/E06_Queries/sol.pl
+++ ArtificalIntelligence/E06_Queries/sol.pl
@@ -1,3 +1,5 @@
+['Restaurants.pl','rules.pl'].
+
 %% Q1
 findall(Rest,branch(Rest,beigang),Z).
 
@@ -8,10 +10,10 @@ setof(Dist,Loc^Rest1^Rest2^Year1^Year2^Loc1^Loc2^(restaurant(Rest1,Year1,yuecai)
 findall(MinRest,(numBranches(MinRest,MinNum),\+setof(SmallerRest,SmallerNum^(numBranches(SmallerRest,SmallerNum),MinNum > SmallerNum),Lst)),Z).
 
 %% Q4
-setof(Rest,branch(Rest,Area),Lst),length(Lst,Len),Len >= 2.
+setof(Area,Lst^Len^(setof(Rest,branch(Rest,Area),Lst),length(Lst,Len),Len >= 2),Z).
 
 %% Q5
-findall(MaxYearRest,(restaurant(MaxYearRest,MaxYear,T2),\+setof(LongerRest,T1^Year^(restaurant(LongerRest,Year,T1),Year > MaxYear),Lst)),Z).
+findall(MinYearRest,(restaurant(MinYearRest,MinYear,T2),\+setof(ShorterRest,T1^Year^(restaurant(ShorterRest,Year,T1),Year < MinYear),Lst)),Z).
 
 %% Q6
-setof(Rest,Num^(numBranches(Rest,Num),Num > 10),Z).
+setof(Rest,Num^(numBranches(Rest,Num),Num >= 10),Z).
