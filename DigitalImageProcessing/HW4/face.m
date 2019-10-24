2bf41069fb20fe5fd7746423d4670c95c94d9049
chhzh123 <30967989+chhzh123@users.noreply.github.com>
2019-10-24 21:03:01 +0800

    [DIP] Update HW4 (face recognition)

--- DigitalImageProcessing/HW4/face.m
+++ DigitalImageProcessing/HW4/face.m
@@ -1,6 +1,8 @@
 close all;clear all;clc;
 
 I = imread('fig/TestImage.jpg');
+I = imread('fig/test1.jfif');
+I = imread('fig/test2.jpg');
 
 BW_rgb = rgb_face(I);
 BW_hsv = hsv_face(I);
@@ -12,30 +14,30 @@ bb_ycbcr = get_bb(BW_ycbcr);
 
 figure, subplot(1,2,2);
 imshow(BW_rgb)
-title('人脸部分标识');
+title('分割图');
 subplot(1,2,1);
-title('人脸识别结果');
 imshow(I);
 hold on;
-rectangle('Position',bb_rgb,'EdgeColor','r')
+rectangle('Position',bb_rgb,'EdgeColor','r');
+title('基于RGB的人脸检测');
 
 figure, subplot(1,2,2);
 imshow(BW_hsv)
-title('人脸部分标识');
+title('分割图');
 subplot(1,2,1);
-title('人脸识别结果');
 imshow(I);
 hold on;
-rectangle('Position',bb_hsv,'EdgeColor','r')
+rectangle('Position',bb_hsv,'EdgeColor','r');
+title('基于HSV的人脸检测');
 
 figure, subplot(1,2,2);
 imshow(BW_ycbcr)
-title('人脸部分标识');
+title('分割图');
 subplot(1,2,1);
-title('人脸识别结果');
 imshow(I);
 hold on;
-rectangle('Position',bb_ycbcr,'EdgeColor','r')
+rectangle('Position',bb_ycbcr,'EdgeColor','r');
+title('基于YCbCr的人脸检测');
 
 function bw = rgb_face(I)
 	[m,n,c] = size(I);
@@ -64,7 +66,7 @@ function bw = hsv_face(I)
 			h = I_h(i,j,1);
 			s = I_h(i,j,2);
 			v = I_h(i,j,3);
-			if (v > 40 && s >= 0.2 && s <= 0.6 && h >= 0 && h <= 0.25)
+			if (v > 0.40 && s >= 0.2 && s <= 0.6 && h >= 0 && h <= 0.25)
 				BW(i,j) = 1;
 			end
 		end
@@ -81,7 +83,7 @@ function bw = ycbcr_face(I)
 			y = I_y(i,j,1);
 			cb = I_y(i,j,2);
 			cr = I_y(i,j,3);
-			if (75 < cb && cb < 250 && 10 < cr && cr < 100 && y > 80)
+			if (75 < cb && cb < 250 && 140 < cr && cr < 160 && y > 80)
 				BW(i,j) = 1;
 			end
 		end
@@ -89,7 +91,7 @@ function bw = ycbcr_face(I)
 	bw = BW;
 end
 
-function [a,b,c,d] = get_bb(BW)
+function g = get_bb(BW)
 	L = bwlabel(BW,8); % 8 connectivity
 	% Left Top Width Height
 	BB = regionprops(L,'BoundingBox'); % get smallest retangle, return as a structure
@@ -100,14 +102,15 @@ function [a,b,c,d] = get_bb(BW)
 	BB1 = struct2cell(BB); % struct to cell
 	BB2 = cell2mat(BB1); % cell to matrix
 
-	[s1 s2] = size(BB2);
-	mx = 0;
+	[s1,s2] = size(BB2);
+	max_area = 0;
+    j = 3;
 	for k = 3:4:s2-1
 	    area_bb = BB2(1,k) * BB2(1,k+1);
-	    if p > max_area && (BB2(1,k) / BB2(1,k+1)) < 1.8
-	        max_area = p;
+	    if area_bb > max_area && (BB2(1,k) / BB2(1,k+1)) < 1.8
+	        max_area = area_bb;
 	        j = k;
 	    end
 	end
-	[a,b,c,d] = [BB2(1,j-2),BB2(1,j-1),BB2(1,j),BB2(1,j+1)]
+	g = [BB2(1,j-2),BB2(1,j-1),BB2(1,j),BB2(1,j+1)];
 end
