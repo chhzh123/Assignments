be21ad72314885fee3a6168bf86e0b95a1699155
chhzh123 <30967989+chhzh123@users.noreply.github.com>
2019-12-14 15:29:41 +0800

    [AI] Update E14 - BP
    Fix gradient update formulas
    Accuracy: 50%

--- ArtificalIntelligence/E14_BP/nn.py
+++ ArtificalIntelligence/E14_BP/nn.py
@@ -10,9 +10,6 @@ class FullyConnectedLayer(object):
       Weight: (in_features, out_features)
       Bias: (out_features)
 
-    By matrix calculus, we have
-    \pd{y}{x} = W
-
     Ref:
     http://cs231n.stanford.edu/vecDerivs.pdf
     """
@@ -26,7 +23,6 @@ class FullyConnectedLayer(object):
         W^{[l]} &\sim \mathcal{N}(\mu=0,\sigma^2 = \frac{1}{n^{[l-1]}})
         b^{[l]} &= 0
         """
-        # self.weight = np.random.rand(in_features,out_features)
         self.weight = np.random.normal(0,np.sqrt(1/in_features),(in_features,out_features))
         if bias:
             self.bias = np.random.rand(out_features)
@@ -45,8 +41,8 @@ class FullyConnectedLayer(object):
 class Network(object):
 
     def __init__(self,in_features,hidden_features,out_features,learning_rate=0.001):
-        self.fc1 = FullyConnectedLayer(in_features,hidden_features,False)
-        self.fc2 = FullyConnectedLayer(hidden_features,out_features,False)
+        self.fc1 = FullyConnectedLayer(in_features,hidden_features,True)
+        self.fc2 = FullyConnectedLayer(hidden_features,out_features,True)
         self.learning_rate = learning_rate
         self.memory = {}
         self.train_flag = True
@@ -62,6 +58,7 @@ class Network(object):
 
     def sigmoid(self,x):
         """
+        Element-wise function
         \Sigma(x) = 1/(1+\ee^{-x})
         """
         return 1 / (1 + np.exp(-x))
@@ -69,35 +66,34 @@ class Network(object):
     def d_sigmoid(self,x):
         return self.sigmoid(x) * (1 - self.sigmoid(x))
 
-    def softmax(self,x):
-        """
-        f(x_i) = e^{x_i} / \sum_j e^{x_j}
-        """
-        return np.exp(x) / np.sum(np.exp(x),axis=0)
-
     def MSE(self,y_hat,y):
-        return np.linalg.norm(y_hat - y)
+        """
+        Mean-square error (MSE)
+        """
+        return np.linalg.norm(y_hat - y) # 2-norm
 
     def forward(self,x):
-        if self. train_flag:
-            self.memory["x0"] = np.copy(x)
+        """
+        w/o activation: z^{(l+1)} = W^{(l)}a^{(l)} + b^{(l)}
+        w/ activation : a^{(l+1)} = f(z^{(l+1)})
+        """
+        if self.train_flag:
+            self.memory["a0"] = np.copy(x)
             # print(x)
-            x = self.fc1(x) # 1 * hidden
+            x = self.fc1(x) # N * hidden
             # print(x)
-            self.memory["Z1"] = np.copy(x)
+            self.memory["z1"] = np.copy(x)
             x = self.sigmoid(x)
             # print(x,self.fc2.weight)
-            self.memory["x1"] = np.copy(x)
-            x = self.fc2(x) # 1 * out
+            self.memory["a1"] = np.copy(x)
+            x = self.fc2(x) # N * out
             # print(x)
-            self.memory["Z2"] = np.copy(x)
+            self.memory["z2"] = np.copy(x)
             x = self.sigmoid(x)
-            # print(x)
-            self.memory["x2"] = np.copy(x)
         else: # test
-            x = self.fc1(x) # 1 * hidden
+            x = self.fc1(x) # N * hidden
             x = self.sigmoid(x)
-            x = self.fc2(x) # 1 * out
+            x = self.fc2(x) # N * out
             x = self.sigmoid(x)
         return x
 
@@ -105,12 +101,36 @@ class Network(object):
         """
         Use MSE as error function
 
-        Ref: https://sudeepraja.github.io/Neural/
+        Ref: http://ufldl.stanford.edu/tutorial/supervised/MultiLayerNeuralNetworks/
         """
-        W2 = self.fc2.weight.copy()
-        delta_2 = (y_hat - y) * self.d_sigmoid(self.memory["Z2"]) # 1 * out_features
-        dW2 = np.dot(self.memory["x1"].reshape(-1,1),delta_2.reshape(1,-1)) # vector outer product -> hidden * out_features
-        self.fc2.weight = self.fc2.weight - self.learning_rate * dW2
-        delta_1 = np.dot(delta_2,W2.T) * self.d_sigmoid(self.memory["Z1"]) # 1 * hidden_features
-        dW1 = np.dot(self.memory["x0"].reshape(-1,1),delta_1.reshape(1,-1)) # in_features * hidden
-        self.fc1.weight = self.fc1.weight - self.learning_rate * dW1
+        # Calculate \delta
+        # output layer: \delta(n_l) = -(y - a(n_l)) * f'(z(n_l))
+        # other layers: \delta(l) = W(l)^T\delta(l+1) * f'(z(l))
+        delta = [0] * 3
+        delta[2] = (y_hat - y) * self.d_sigmoid(self.memory["z2"]) # N * out_features
+        delta[1] = np.dot(delta[2],self.fc2.weight.T) * self.d_sigmoid(self.memory["z1"]) # N * hidden_features
+
+        # Calculat \nabla
+        # output layer: \nabla_{W(l)}J(W,b;x,y) = \delta(l+1)(a(l))^T # outer product
+        # other layers: \nabla_{b(l)}J(W,b;x,y) = \delta(l+1)
+        nabla_W = [0] * 2
+        nabla_W[1] = np.einsum("ij,ik->ikj",delta[2],self.memory["a1"]) # N * hidden_features * out_features
+        nabla_W[0] = np.einsum("ij,ik->ikj",delta[1],self.memory["a0"]) # N * in_features * hidden_features
+        nabla_b = [0] * 2
+        nabla_b[1] = delta[2] # N * out_features
+        nabla_b[0] = delta[1] # N * hidden_features
+
+        # Update parameters
+        # W(l) = W(l) - \alpha((1/m \Delta W(l)) + \lambda W(l))
+        # b(l) = b(l) - \alpha(1/m \Delta b(l))
+        # Use einsum to accelerate
+        # https://rockt.github.io/2018/04/30/einsum
+        nabla_W[1] = nabla_W[1].mean(axis=0)
+        nabla_W[0] = nabla_W[0].mean(axis=0)
+        nabla_b[1] = nabla_b[1].mean(axis=0)
+        nabla_b[0] = nabla_b[0].mean(axis=0)
+
+        self.fc2.weight = self.fc2.weight - self.learning_rate * nabla_W[1]
+        self.fc1.weight = self.fc1.weight - self.learning_rate * nabla_W[0]
+        self.fc2.bias = self.fc2.bias - self.learning_rate * nabla_b[1]
+        self.fc1.bias = self.fc1.bias - self.learning_rate * nabla_b[0]
