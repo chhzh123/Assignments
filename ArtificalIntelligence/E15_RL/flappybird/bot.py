138bcc026d1e5f51d7329ae0fd4cc26d888b19e4
chhzh123 <30967989+chhzh123@users.noreply.github.com>
2019-12-19 09:56:59 +0800

    [AI] Update E15 - RL (flappy bird)

---
+++ ArtificalIntelligence/E15_RL/flappybird/bot.py
@@ -0,0 +1,120 @@
+import sys
+import json
+
+class Bot(object):
+    """
+    The Bot class that applies the Qlearning logic to Flappy bird game
+    After every iteration (iteration = 1 game that ends with the bird dying) updates Q values
+    After every DUMPING_N iterations, dumps the Q values to the local JSON file
+    """
+
+    def __init__(self):
+        self.gameCNT = 0  # Game count of current run, incremented after every death
+        self.DUMPING_N = 25  # Number of iterations to dump Q values to JSON after
+        self.discount = 1.0
+        self.r = {0: 1, 1: -1000}  # Reward function
+        self.lr = 0.7
+        self.load_qvalues()
+        self.last_state = "420_240_0"
+        self.last_action = 0
+        self.moves = []
+
+    def load_qvalues(self):
+        """
+        Load q values from a JSON file
+        """
+        self.qvalues = {}
+        try:
+            fil = open("qvalues.json", "r")
+        except IOError:
+            return
+        self.qvalues = json.load(fil)
+        fil.close()
+
+    def act(self, xdif, ydif, vel):
+        """
+        Chooses the best action with respect to the current state - Chooses 0 (don't flap) to tie-break
+        """
+        state = self.map_state(xdif, ydif, vel)
+
+        self.moves.append(
+            (self.last_state, self.last_action, state)
+        )  # Add the experience to the history
+
+        self.last_state = state  # Update the last_state with the current state
+
+        if self.qvalues[state][0] >= self.qvalues[state][1]:
+            self.last_action = 0
+            return 0
+        else:
+            self.last_action = 1
+            return 1
+
+    def update_scores(self, dump_qvalues = True):
+        """
+        Update qvalues via iterating over experiences
+        """
+        history = list(reversed(self.moves))
+
+        # Flag if the bird died in the top pipe
+        high_death_flag = True if int(history[0][2].split("_")[1]) > 120 else False
+
+        # Q-learning score updates
+        for i,(last_state, action, state) in enumerate(history):
+
+            # Select reward
+            # 0: still alive  1: game over
+            if i in [0,1]:
+                # last state has been the "game over" state
+                # thus the last second that leads to the "game over" state should also receive negative rewards
+                reward = self.r[1]
+            elif high_death_flag and action == 1:
+                # if the bird died in the top pipe
+                # then the previous action should be 1 (flip up)
+                reward = self.r[1]
+                high_death_flag = False
+            else:
+                # otherwise, the bird is alive
+                reward = self.r[0]
+
+            # find the max q value
+            maxq = max(self.qvalues[state][0],self.qvalues[state][1])
+
+            # Update self.qvalues[state][act]
+            # Balance exploitation and exploration
+            self.qvalues[last_state][action] = (1 - self.lr) * self.qvalues[last_state][action] + self.lr * (reward + self.discount * maxq)
+
+        self.gameCNT += 1  # increase game count
+        if dump_qvalues:
+            self.dump_qvalues()  # Dump q values (if game count % DUMPING_N == 0)
+        self.moves = []  # clear history after updating strategies
+
+    def map_state(self, xdif, ydif, vel):
+        """
+        Map the (xdif, ydif, vel) to the respective state, with regards to the grids
+        The state is a string, "xdif_ydif_vel"
+
+        X -> [-40,-30...120] U [140, 210 ... 420]
+        Y -> [-300, -290 ... 160] U [180, 240 ... 420]
+        """
+        if xdif < 140:
+            xdif = int(xdif) - (int(xdif) % 10)
+        else:
+            xdif = int(xdif) - (int(xdif) % 70)
+
+        if ydif < 180:
+            ydif = int(ydif) - (int(ydif) % 10)
+        else:
+            ydif = int(ydif) - (int(ydif) % 60)
+
+        return str(int(xdif)) + "_" + str(int(ydif)) + "_" + str(vel)
+
+    def dump_qvalues(self, force = False):
+        """
+        Dump the qvalues to the JSON file
+        """
+        if self.gameCNT % self.DUMPING_N == 0 or force:
+            fil = open("qvalues.json", "w")
+            json.dump(self.qvalues, fil)
+            fil.close()
+            print("Q-values updated on local file.")
