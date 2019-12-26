85a546831c3b4164420ca47f90d7fec22d446082
chhzh123 <30967989+chhzh123@users.noreply.github.com>
2019-12-26 15:11:11 +0800

    [AI] E16 - DL (update report)

--- ArtificalIntelligence/E16_DL/ConvNet.py
+++ ArtificalIntelligence/E16_DL/ConvNet.py
@@ -1,24 +1,18 @@
-# As usual, a bit of setup
-# import numpy as np
-# import matplotlib.pyplot as plt
 from cs231n.classifiers.cnn import *
 from cs231n.data_utils import get_CIFAR10_data
-# from cs231n.gradient_check import eval_numerical_gradient_array, eval_numerical_gradient
-# from cs231n.layers import *
-# from cs231n.fast_layers import *
 from cs231n.solver import Solver
 
-# reading the input images
 data = get_CIFAR10_data()
 for k, v in data.items():
-    print('%s: ' % k, v.shape)
+    print('{}: '.format(k), v.shape)
+
 
 # set up model
 model = ThreeLayerConvNet(hidden_dim=512, reg=0.001)
 
 # encapsulate it into solver
 solver = Solver(model, data,
-                num_epochs=3,
+                num_epochs=1,
                 batch_size=50,
                 update_rule='adam',
                 optim_config={
@@ -28,3 +22,16 @@ solver = Solver(model, data,
                 print_every=50,
                 checkpoint_name="checkpoint/cnn")
 solver.train()
+
+
+solver.check_accuracy(data["X_test"],data["y_test"])
+
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlabel("Iteration")
+ax.set_ylabel("Loss")
+ax.plot(solver.loss_history)
+fig.savefig("fig/loss.pdf",format="pdf")
