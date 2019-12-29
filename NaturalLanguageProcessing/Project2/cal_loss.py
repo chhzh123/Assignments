cfdceb44d8835df1460d5b09324882dd8f24a57a
chhzh123 <30967989+chhzh123@users.noreply.github.com>
2019-12-29 17:13:51 +0800

    [NLP] Update Project 2
    1. Attention
    2. BLEU
    3. Beam Search
    4. Loss from log

---
+++ NaturalLanguageProcessing/Project2/cal_loss.py
@@ -0,0 +1,21 @@
+import sys
+import matplotlib.pyplot as plt
+
+file_num = int(sys.argv[1])
+draw_every = int(sys.argv[2])
+
+cnt = 0
+loss = 0
+losses = []
+with open("log/seq2seq-{:>02d}.log".format(file_num),"r") as file:
+	for i,line in enumerate(file):
+		if "Epoch" not in line:
+			cnt += 1
+			continue
+		loss += float(line.split("Loss: ")[-1])
+		if (i-cnt) % draw_every == 0:
+			losses.append(loss/draw_every)
+			loss = 0
+
+plt.plot(losses)
+plt.show()
