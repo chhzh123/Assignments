5c8ce8e548e781a058f95d76514c79c15023c229
chhzh123 <982270930@qq.com>
2020-05-12 23:16:19 +0800

    [CP] Finish Lab 02 - Flex

---
+++ Compilers/Lab02/lex_test.cpp
@@ -0,0 +1,31 @@
+#include <cctype>
+#include <cstdio>
+using namespace std;
+
+/*
+ * A large comment
+ * with numbers
+ * 1, 2,3, 4, 5...
+ */
+
+#define PI 3.14159E0
+typedef __int8_t myuint8;
+
+struct A
+{
+	int val1 = -1;
+	float val_2 = 100.1001;
+};
+
+int main() {
+	/* inline f12s 234 14.234 */
+	int exp = (1 +3)* 2 / 4; // expressions
+	int asf24e =45;
+	float _123ab = -0.1;
+	printf("Print a float %f\n", 10e-5);
+	for (int i = 10; i>-2;--i);
+	A a;
+	a.val1 = 404;
+	a.val_2 = 99.9;
+	return 0;
+}
