1872dd0da01b61bafca359c8464a20df58a69719
chhzh123 <982270930@qq.com>
2020-07-19 15:15:36 +0800

    [CP] Final (NFA2DFA)

--- Compilers/Final/main.cpp
+++ Compilers/Final/main.cpp
@@ -4,6 +4,9 @@
 #include <stack>
 #include <map>
 #include <vector>
+#include <queue>
+#include <set>
+#include <utility>
 using namespace std;
 
 /*
@@ -95,16 +98,15 @@ public:
 };
 
 // McNaughton-Yamada-Thompson algorithm
-void regex2nfa(string str) {
+pair<NFA_Node*,NFA_Node*> regex2nfa(const string str, vector<NFA_Node*>& nfa) {
 	stack<NFA_Node*> autostk;
-	vector<NFA_Node*> nfa;
 	for (auto c : str) {
 		if (isalpha(c)) {
 			NFA_Node* begin = new NFA_Node();
 			NFA_Node* end = new NFA_Node();
 			nfa.push_back(begin);
 			nfa.push_back(end);
-			cout << c << " " << begin->id << " " << end->id << endl;
+			// cout << c << " " << begin->id << " " << end->id << endl;
 			if (c != 'E')
 				begin->out[c] = end->id;
 			else
@@ -159,30 +161,145 @@ void regex2nfa(string str) {
 		} else if (c == '+') {}
 	}
 	for (auto state : nfa) {
-		cout << state->id << ": ";
+		cout << state->id;
+		if (state->accepting)
+			cout << "(A)";
+		cout << ": ";
 		for (auto edge : state->e)
 			cout << edge << " ";
-		cout << endl << "\t";
 		for (auto& c : state->out)
 			cout << c.first << "(" << c.second << ")";
 		cout << endl;
 	}
+	NFA_Node* end = autostk.top(); autostk.pop();
+	NFA_Node* begin = autostk.top(); autostk.pop();
+	pair<NFA_Node*, NFA_Node*> res(begin,end);
+	return res;
+}
+
+void traverse_e(NFA_Node* s, const vector<NFA_Node*>& nfa, set<int>& res) {
+	for (auto neigh : s->e) {
+		res.insert(neigh);
+		traverse_e(nfa[neigh], nfa, res);
+	}
+}
+
+set<int> epsilon_closure(NFA_Node* s, const vector<NFA_Node*>& nfa) {
+	set<int> res;
+	res.insert(s->id);
+	traverse_e(s,nfa,res);
+	return res;
+}
+
+set<int> epsilon_closure(const set<int>& T, const vector<NFA_Node*>& nfa) {
+	set<int> res;
+	for (auto s : T) {
+		set<int> tmp = epsilon_closure(nfa[s],nfa);
+		res.insert(tmp.begin(),tmp.end());
+	}
+	return res;
+}
+
+void traverse(NFA_Node* s, const vector<NFA_Node*>& nfa, const char a, set<int>& res) {
+	if (s->out.count(a) != 0)
+		res.insert(s->out[a]);
+}
+
+set<int> move_to(const set<int>& T, const vector<NFA_Node*>& nfa, const char a) {
+	set<int> res;
+	for (auto s : T)
+		traverse(nfa[s],nfa,a,res);
+	return res;
+}
+
+class DFA_Node{
+public:
+	DFA_Node() : id(cnt), accepting(false) {
+		cnt++;
+	}
+	int id;
+	static int cnt;
+	bool accepting;
+	map<char,int> out;
+};
+
+template<typename T>
+void print_set(const set<T>& s, bool newline=true) {
+	cout << "Set: ";
+	for (auto x : s)
+		cout << x << " ";
+	if (newline)
+		cout << endl;
+}
+
+void nfa2dfa(const pair<NFA_Node*,NFA_Node*>& p,
+			 const vector<NFA_Node*>& nfa,
+			 const set<char>& input_symbol,
+			 vector<DFA_Node*>& dfa) {
+	NFA_Node* start = p.first;
+	NFA_Node* end = p.second;
+	// cout << start->id << " " << end->id << endl;
+	queue<set<int>> q;
+	set<int> start_closure = epsilon_closure(start,nfa);
+	q.push(start_closure);
+	DFA_Node* node = new DFA_Node();
+	dfa.push_back(node);
+	set<set<int>> marked;
+	map<set<int>,int> lut;
+	lut[q.front()] = 0;
+	while (!q.empty()) {
+		set<int> s = q.front();
+		marked.insert(s);
+		int len = dfa.size();
+		for (auto sym : input_symbol) {
+			set<int> move = move_to(s,nfa,sym);
+			if (move.empty())
+				continue;
+			set<int> U = epsilon_closure(move,nfa);
+			cout << sym << " ";
+			print_set<int>(move,false);
+			cout << " | ";
+			print_set<int>(U);
+			if (marked.find(U) == marked.end()) { // U not in Dstates
+				q.push(U);
+				DFA_Node* node = new DFA_Node();
+				dfa.push_back(node);
+				lut[U] = dfa.size() - 1;
+				cout << "Add state: ^ " << dfa.size() - 1 << endl;
+			}
+			dfa[len-1]->out[sym] = lut[U];
+		}
+		q.pop();
+	}
+	for (auto& item : lut) {
+		print_set<int>(item.first,false);
+		cout << " -> " << item.second << endl;
+	}
 }
 
 int NFA_Node::cnt = 0;
+int DFA_Node::cnt = 0;
 
 // 3.9 Optimization of DFA-Based Pattern Matchers
 int main() {
 	string str;
 	// cin >> str;
 	// str = "(ab)*c+(d|e)?";
-	str = "(a|b)*cd";
+	// str = "(a|b)*cd";
+	str = "(a|b)*abb";
 	// str += "#";
 	str = insert_concat(str);
 	cout << str << endl;
 	str = infix2postfix(str);
 	cout << str << endl;
-	regex2nfa(str);
+	vector<NFA_Node*> nfa;
+	pair<NFA_Node*, NFA_Node*> p;
+	p = regex2nfa(str,nfa);
+	set<char> input_symbol(str.begin(),str.end());
+	input_symbol.erase('.');
+	print_set<char>(input_symbol);
+	vector<DFA_Node*> dfa;
+	nfa2dfa(p,nfa,input_symbol,dfa);
 	return 0;
 }
 
