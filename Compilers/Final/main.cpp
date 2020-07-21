d51b70caf5c1a1c1e95289fdfe18cec935750482
chhzh123 <982270930@qq.com>
2020-07-21 10:34:10 +0800

    [CP] Final (Add + & Fix DFS bug)

--- Compilers/Final/main.cpp
+++ Compilers/Final/main.cpp
@@ -99,6 +99,7 @@ public:
 
 // 3.7.4 Construction of an NFA from a Regular Expression
 // McNaughton-Yamada-Thompson algorithm
+// https://github.com/Ronan-H/regex-nfa-builder/blob/master/nfa_utils.py
 pair<NFA_Node*,NFA_Node*> regex2nfa(const string str, vector<NFA_Node*>& nfa) {
 	stack<NFA_Node*> autostk;
 	for (auto c : str) {
@@ -134,8 +135,10 @@ pair<NFA_Node*,NFA_Node*> regex2nfa(const string str, vector<NFA_Node*>& nfa) {
 			autostk.push(begin);
 			autostk.push(end);
 		} else if (c == '.') { // concatenation
+			// N(t)
 			NFA_Node* d = autostk.top(); autostk.pop();
 			NFA_Node* c = autostk.top(); autostk.pop();
+			// N(s)
 			NFA_Node* b = autostk.top(); autostk.pop();
 			NFA_Node* a = autostk.top(); autostk.pop();
 			b->e.push_back(c->id);
@@ -158,8 +161,22 @@ pair<NFA_Node*,NFA_Node*> regex2nfa(const string str, vector<NFA_Node*>& nfa) {
 			end->accepting = true;
 			autostk.push(begin);
 			autostk.push(end);
-		} else if (c == '?') {
-		} else if (c == '+') {}
+		} else if (c == '?') { // zero or one, E|N(t)
+			NFA_Node* begin = new NFA_Node();
+			NFA_Node* end = new NFA_Node();
+			nfa.push_back(begin);
+			nfa.push_back(end);
+			NFA_Node* b = autostk.top(); autostk.pop();
+			NFA_Node* a = autostk.top(); autostk.pop();
+			begin->e.push_back(a->id);
+			begin->e.push_back(end->id);
+			b->e.push_back(end->id);
+			b->accepting = false;
+			end->accepting = true;
+			autostk.push(begin);
+			autostk.push(end);
+		} else if (c == '+') {
+		}
 	}
 	for (auto state : nfa) {
 		cout << state->id;
@@ -224,7 +241,7 @@ public:
 	static int cnt;
 	bool start;
 	bool accepting;
-	map<char,int> out;
+	map<char,int> out; // be careful of non-existed keys
 	int group;
 };
 
@@ -237,8 +254,16 @@ void print_set(const set<T>& s, bool newline=true) {
 		cout << endl;
 }
 
+template<typename T>
+void print_vector(const vector<T> v) {
+	cout << "Vector: ";
+	for (auto x : v)
+		cout << x << " ";
+	cout << endl;
+}
+
 // 3.7.1 Conversion of an NFA to a DFA
-void nfa2dfa(const pair<NFA_Node*,NFA_Node*>& p,
+int nfa2dfa(const pair<NFA_Node*,NFA_Node*>& p,
 			 const vector<NFA_Node*>& nfa,
 			 const set<char>& input_symbol,
 			 vector<DFA_Node*>& dfa) {
@@ -282,23 +307,27 @@ void nfa2dfa(const pair<NFA_Node*,NFA_Node*>& p,
 	for (auto& item : lut) {
 		int idx = item.second;
 		if (item.first.count(end->id) != 0) {
-			cout << end->id << " " << item.second << endl;
 			dfa[idx]->accepting = true;
 			dfa[idx]->group = 0;
 		}
 		if (item.first.count(start->id) != 0) {
 			dfa[idx]->start = true;
+			dfa[idx]->group = 1;
 		}
 	}
+	int res_start;
 	for (auto& item : lut) {
 		print_set<int>(item.first,false);
 		cout << " -> " << item.second;
-		if (dfa[item.second]->start)
+		if (dfa[item.second]->start) {
 			cout << "(S)";
+			res_start = item.second;
+		}
 		if (dfa[item.second]->accepting)
 			cout << "(A)";
 		cout << endl;
 	}
+	return res_start;
 }
 
 // 3.9.6 Minimizing the Number of States of a DFA
@@ -308,18 +337,23 @@ int minimize_dfa(vector<DFA_Node*>& dfa,
 	queue<vector<int>> partition;
 	vector<int> s1, s2;
 	for (auto state : dfa) {
-		if (state->group)
+		// if (state->group)
+		// 	s1.push_back(state->id);
+		// else // accepting state
+		// 	s2.push_back(state->id);
+		if (state->start)
 			s1.push_back(state->id);
-		else // accepting state
+		else
 			s2.push_back(state->id);
 	}
 	int n_group = 2;
-	partition.push(s2); // suppose accepting states are in the same group
+	partition.push(s2);
 	partition.push(s1);
 	map<int,int> state_map;
 	set<int> group_id = {0,1};
 	while (!partition.empty()) {
 		vector<int> p = partition.front();
+		print_vector<int>(p);
 		partition.pop();
 		int size = p.size();
 		if (size < 2)
@@ -328,7 +362,7 @@ int minimize_dfa(vector<DFA_Node*>& dfa,
 		for (auto c : input_symbol) {
 			map<int,vector<int>> groups; // gid, idx in group
 			for (int i = 0; i < size; ++i) {
-				int out = dfa[p[i]]->out[c];
+				int out = dfa[p[i]]->out.at(c);
 				groups[dfa[out]->group].push_back(i);
 			}
 			int g_size = groups.size();
@@ -373,7 +407,7 @@ int minimize_dfa(vector<DFA_Node*>& dfa,
 	for (int i = 0; i < n_group; ++i) {
 		DFA_Node* node = new DFA_Node();
 		for (auto c : input_symbol) {
-			int out = dfa[state_map[i]]->out[c];
+			int out = dfa[state_map[i]]->out.at(c);
 			node->out[c] = dfa[out]->group;
 		}
 		if (start_flag[i])
@@ -399,7 +433,10 @@ void print_dfa(vector<DFA_Node*>& dfa, set<char>& input_symbol) {
 		cout << "\t";
 		for (auto c : input_symbol)
 			// cout << (char)(node->out[c]+'A') << "\t";
-			cout << node->out[c] << "\t";
+			if (node->out.count(c) == 0)
+				cout << (-1) << "\t";
+			else
+				cout << node->out.at(c) << "\t";
 		cout << endl;
 	}
 }
@@ -431,37 +468,44 @@ int build_dfa(string str, set<char>& input_symbol,vector<DFA_Node*>& min_dfa) {
 	p = regex2nfa(str,nfa);
 	DFA_Node::cnt = 0;
 	vector<DFA_Node*> dfa;
-	nfa2dfa(p,nfa,input_symbol,dfa);
+	int start_dfa = nfa2dfa(p,nfa,input_symbol,dfa);
 	print_dfa(dfa,input_symbol);
+	min_dfa = dfa;
+	return start_dfa;
 	DFA_Node::cnt = 0;
-	int start = minimize_dfa(dfa,input_symbol,min_dfa);
+	int start_mindfa = minimize_dfa(dfa,input_symbol,min_dfa);
 	print_dfa(min_dfa,input_symbol);
-	return start;
+	return start_mindfa;
 }
 
-bool contain(int s1, int s2,
+bool intersection(int s1, int s2,
 			 vector<DFA_Node*>& dfa1,
 			 vector<DFA_Node*>& dfa2,
+			 int len1, int len2,
 			 set<char>& input_symbol,
 			 vector<vector<bool>>& visited) {
-	if (visited[s1][s2])
-		return true;
 	visited[s1][s2] = true;
-	bool acc1 = dfa1[s1]->accepting;
-	bool acc2 = dfa2[s2]->accepting;
+	bool acc1 = (s1 == len1) ? false : dfa1[s1]->accepting;
+	bool acc2 = (s2 == len2) ? true : dfa2[s2]->accepting;
+	cout << s1 << " " << acc1 << " " << s2 << " " << acc2 << endl;
 	if (acc1 && acc2)
 		return true;
-	else if (acc1 || acc2)
-		return false;
 	for (auto c : input_symbol) {
-		if (dfa2[s2]->out.find(c) == dfa2[s2]->out.end())
-			return false;
-		int o1 = dfa1[s1]->out[c];
-		int o2 = dfa2[s2]->out[c];
-		if (!contain(o1,o2,dfa1,dfa2,input_symbol,visited))
-			return false;
+		int o1 = (s1 == len1 || dfa1[s1]->out.count(c) == 0) ?
+				  len1 : dfa1[s1]->out.at(c);
+		int o2 = (s2 == len2 || dfa2[s2]->out.count(c) == 0) ?
+				  len2 : dfa2[s2]->out.at(c);
+		if (!visited[o1][o2])
+			if (intersection(o1,o2,dfa1,dfa2,len1,len2,input_symbol,visited))
+				return true;
 	}
-	return true;
+	return false;
+}
+
+void complement(vector<DFA_Node*>& dfa) {
+	for (auto node : dfa) {
+		node->accepting = !node->accepting;
+	}
 }
 
 int judge(vector<DFA_Node*>& dfa1, vector<DFA_Node*>& dfa2,
@@ -469,27 +513,64 @@ int judge(vector<DFA_Node*>& dfa1, vector<DFA_Node*>& dfa2,
 		  int s1, int s2) {
 	cout << "start: " << s1 << " " << s2 << endl;
 	vector<vector<bool>> visited;
+	// add a dummy state
 	int len1 = dfa1.size();
 	int len2 = dfa2.size();
-	for (int i = 0; i < len1; ++i) {
-		vector<bool> tmp(len2,false);
+	for (int i = 0; i < len1+1; ++i) {
+		vector<bool> tmp(len2+1,false);
 		visited.push_back(tmp);
 	}
-	bool a_in_b = contain(s1,s2,dfa1,dfa2,symbol1,visited);
+	complement(dfa2);
+	print_dfa(dfa1,symbol1);
+	print_dfa(dfa2,symbol2);
+	bool a_in_cb = intersection(s1,s2,dfa1,dfa2,len1,len2,symbol1,visited);
+	cout << endl;
 	visited.clear();
-	for (int i = 0; i < len2; ++i) {
-		vector<bool> tmp(len1,false);
+	for (int i = 0; i < len2+1; ++i) {
+		vector<bool> tmp(len1+1,false);
 		visited.push_back(tmp);
 	}
-	bool b_in_a = contain(s2,s1,dfa2,dfa1,symbol2,visited);
-	if (a_in_b && b_in_a)
+	complement(dfa2);
+	complement(dfa1);
+	// print_dfa(dfa1,symbol1);
+	bool b_in_ca = intersection(s2,s1,dfa2,dfa1,len2,len1,symbol2,visited);
+	if (!a_in_cb && !b_in_ca)
 		return 0;
-	else if (a_in_b && !b_in_a)
+	else if (!a_in_cb && b_in_ca)
 		return 1;
-	else if (!a_in_b && b_in_a)
+	else if (a_in_cb && !b_in_ca)
 		return 2;
 	else
 		return 3;
+}
+
+string insert_plus(const string str) {
+	// cannot handle "((x|y)+)+"
+	if (str.find("+") == string::npos)
+		return str;
+	string res = "";
+	stack<int> stk;
+	int len = str.size();
+	for (int i = 0; i < len; ++i) {
+		if (str[i] == '(') {
+			stk.push(i);
+			res += "(";
+		} else if (str[i] == ')') {
+			if (i + 1 < len && str[i+1] == '+') {
+				int front = stk.top();
+				string new_str = str.substr(front,i-front+1);
+				res += ")" + new_str + "*";
+				stk.pop();
+			} else
+				res += ")";
+		} else if (str[i] == '+' && str[i-1] != ')') {
+			char c = str[i-1];
+			res += c;
+			res += "*";
+		} else if (str[i] != '+')
+			res += str[i];
+	}
+	return res;
 }
 
 int NFA_Node::cnt = 0;
@@ -508,9 +589,27 @@ int main() {
 		// str = "(a|b)*cd";
 		// str1 = "(a|b)*abb";
 		// str2 = "(a|b)*abb";
-		str1 = "((E|a)b*)*";
-		str2 = "(a|b)*";
-		// str += "#";
+		// str1 = "a";
+		// str2 = "a+";
+		// case 1 =
+		// str1 = "((E|a)b*)*";
+		// str2 = "(a|b)*";
+		// case 2 =
+		// str1 = "b*a*b?a*";
+		// str2 = "b*a*ba*|b*a*";
+		// case 3 >
+		// str1 = "b*a*b?a*";
+		// str2 = "(b*|a*)(b|E)a*";
+		// case 4 !
+		// str1 = "(c|d)*c(c|d)(c|d)";
+		// str2 = "(c|d)*d(c|d)(c|d)";
+		// case 5 <
+		str1 = "x+y+z+";
+		str2 = "x*y*z*";
+		str1 = insert_plus(str1);
+		str2 = insert_plus(str2);
+		// cout << str1 << endl << str2 << endl;
+		// abort();
 		str1 = get_postfix(str1);
 		str2 = get_postfix(str2);
 		set<char> symbol1 = get_input_symbol(str1);
@@ -538,6 +637,9 @@ int main() {
 // b*a*b?a* (b*|a*)(b|E)a* >
 // (c|d)*c(c|d)(c|d) (c|d)*d(c|d)(c|d) !
 // x+y+z+ x*y*z* <
+
+// str1 = "(x|y)+(y+b*)*";
+// str2 = "((x|y)+)+";
 
 // https://runestone.academy/runestone/books/published/pythonds/BasicDS/InfixPrefixandPostfixExpressions.html
 // http://www.cppblog.com/woaidongmao/archive/2010/09/05/97541.html
